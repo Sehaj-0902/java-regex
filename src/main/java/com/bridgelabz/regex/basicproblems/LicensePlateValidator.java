fd4cf78dff18d4751b5e595ae3f9c2412dd259dd
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-04-02 00:49:17 +0530

    [Sehaj] ADD: Program to validate license plate number

---
+++ src/main/java/com/bridgelabz/regex/basicproblems/LicensePlateValidator.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.regex.basicproblems;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class LicensePlateValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter license plate number: ");
+        String licenseNumber = sc.nextLine();
+
+        isValidLicensePlateNumber(licenseNumber);
+
+        if (isValidLicensePlateNumber(licenseNumber)) {
+            System.out.println("The license plate number " + licenseNumber + " is valid.");
+        }
+        else {
+            System.out.println("The license plate number " + licenseNumber + " is invalid.");
+        }
+    }
+
+    // Method to validate license plate number
+    public static boolean isValidLicensePlateNumber(String licenseNumber) {
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(licenseNumber).matches();
+    }
+}
+
+/*
+Input:
+    Enter license plate number: AB1234
+Output:
+    The license plate number AB1234 is valid.
+
+Input:
+    Enter license plate number: A12345
+Output:
+    The license plate number A12345 is invalid.
+ */
